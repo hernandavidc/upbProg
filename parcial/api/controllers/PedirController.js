568c54b301412ec7c496826535f0a2acf62918a6
hernandavidc <hernandavidc@hotmail.com>
2017-09-04 11:48:32 -0500

    rr

--- parcial/api/controllers/PedirController.js
+++ parcial/api/controllers/PedirController.js
@@ -10,21 +10,11 @@ module.exports = {
    {
 
 		 var http = require('http');
-		 var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=google';
-
-		 http.get(url, function(res) {
-		      var body = '';
-					var result = '';
-		      res.on('data', function(chunk) {
-		         body += chunk;
-		      });
-		      res.on('end', function() {
-		        result = JSON.parse(body)
-						responder(result);
-		      });
-		    }).on('error', function(e) {
-		      console.log("Error: " + e.message);
-		 });
+		 var rutas = ['http://10.152.164.150:3000/', 'http://127.0.0.1:3000/blog'];
+		 var result = '';
+		 rutas.forEach(function(ruta) {
+			    result = pedir(ruta);
+			});
 		 /**
 		var request = require('request');
 
@@ -38,9 +28,24 @@ module.exports = {
 		    sails.log.info(response);
 		  }
 		});*/
+		function pedir(url){
+			http.get(url, function(res) {
+ 		      var body = '';
+ 					var result = '';
+ 		      res.on('data', function(chunk) {
+ 		         body += chunk;
+ 		      });
+ 		      res.on('end', function() {
+ 		        result = JSON.parse(body)
+ 						responder(result);
+ 		      });
+ 		    }).on('error', function(e) {
+ 		      console.log("Error: " + e.message);
+ 		 });
+		}
 			function responder(result){
-				console.log(result);
-				return res.json(result);
+				sails.log(result);
+				return result;
 			}
    }
 };
